4d0ad29ddc80657083c02c01537a61bdfdda21f0
Rishabh Singh <ayushsingh91200@gmail.com>
2024-10-13 15:26:06 +0530

    13-10-2024

--- GLBITM MCA PROGRAMS/C-PROGRAMS-GLBITM/sum_avg.c
+++ GLBITM MCA PROGRAMS/C-PROGRAMS-GLBITM/sum_avg.c
@@ -2,4 +2,9 @@
 #include<stdio.h>
 int main(){
     int a,b,c;
+    printf("Eneter a,b,c: ");
+    scanf("%d%d%d",&a,&b,&c);
+    printf("Sum of a,b,c = %d",a+b+c);
+    printf("\nAvg:%d",(a+b+c)/3);
+    return 0;
 }
