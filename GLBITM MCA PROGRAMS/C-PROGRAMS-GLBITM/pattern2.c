d58f68a64a2c8eab9990dea0acef97e4c707b899
Rishabh Singh <ayushsingh91200@gmail.com>
2024-10-13 17:51:56 +0530

    pattern2 using for loop

---
+++ GLBITM MCA PROGRAMS/C-PROGRAMS-GLBITM/pattern2.c
@@ -0,0 +1,16 @@
+//  1
+//  12
+//  123
+#include<stdio.h>
+int main(){
+    int row;
+    printf("Enter the rows: ");
+    scanf("%d",&row);
+    for(int i=1;i<=row;i++){
+        for(int j=1;j<=i;j++){
+        printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
