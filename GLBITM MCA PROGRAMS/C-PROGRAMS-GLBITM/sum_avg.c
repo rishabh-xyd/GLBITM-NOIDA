78a5c0cef8920612cf07f5bfa3577c363ec5b554
Rishabh Singh <ayushsingh91200@gmail.com>
2024-10-13 17:05:39 +0530

    updated

--- GLBITM MCA PROGRAMS/C-PROGRAMS-GLBITM/sum_avg.c
+++ GLBITM MCA PROGRAMS/C-PROGRAMS-GLBITM/sum_avg.c
@@ -1,10 +1,18 @@
-//WAP to read number from user and find there sum
+//WAP to read number from user and find there sum and the avg
 #include<stdio.h>
 int main(){
-    int a,b,c;
-    printf("Eneter a,b,c: ");
-    scanf("%d%d%d",&a,&b,&c);
-    printf("Sum of a,b,c = %d",a+b+c);
-    printf("\nAvg:%d",(a+b+c)/3);
+    int num,r,count=0,sum = 0,i,avg;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    for(i=1;i<=num;i++)
+    {
+        r=num%10;
+        num/=10;
+        sum=sum+r;
+    
+    }
+        printf("Sum of digits of the number is: %d", sum);
+        avg=sum/(i-1);
+        printf("\nAvg of digits of the number is: %d", avg);
     return 0;
 }
